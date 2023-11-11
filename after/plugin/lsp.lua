dd43cd83a1aceb33adcaffabfdf624add384e3a1
Joshua Turner <joshua.turner@frcteamreboot.com>
2023-11-11 17:18:26 -0500

    migrate lsp-zero to v3

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,35 +1,8 @@
-local lsp = require('lsp-zero').preset({
-    name = 'minimal',
-    set_lsp_keymaps = true,
-    manage_nvim_cmp = true,
-    suggest_lsp_servers = false,
-})
+local lsp_zero = require('lsp-zero')
 
-
-lsp.ensure_installed({
-    'rust_analyzer',
-    'bashls',
-    'marksman',
-    'lua_ls'
-})
-
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
-
-local function virtual_text_on()
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = true,
-            signs = true,
-            underline = true,
-            update_in_insert = false,
-        })
-    vim.g.diagnostic_virtual_txt = true
-    vim.diagnostic.show(nil, 0, nil, { virtual_text = true })
-end
-
-lsp.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
+	lsp_zero.default_keymaps({buffer = bufnr})
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -43,23 +16,89 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
-lsp.set_sign_icons({
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {},
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require('lspconfig').lua_ls.setup(lua_opts)
+    end,
+	typst_lsp = function ()
+		require('lspconfig').typst_lsp.setup {
+			settings = {
+				-- Using typst watch <file name> so this can be never.
+				exportPdf = "never" -- Choose onType, onSave or never.
+			}
+		}
+	end
+  }
+})
+
+lsp_zero.set_sign_icons({
   error = '🤬',
   warn = '🤢',
   hint = '⚑',
   info = '»'
 })
 
-lsp.setup()
+vim.diagnostic.config({
+  virtual_text = true,
+  severity_sort = true,
+  float = {
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+})
 
+local cmp = require('cmp')
+local cmp_action = lsp_zero.cmp_action()
+local cmp_format = lsp_zero.cmp_format()
 
--- for typst lsp
-require'lspconfig'.typst_lsp.setup{
-	settings = {
-        -- Using typst watch <file name> so this can be never.
-		exportPdf = "never" -- Choose onType, onSave or never.
-	}
-}
+require('luasnip.loaders.from_vscode').lazy_load()
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+cmp.setup({
+  formatting = cmp_format,
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  window = {
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'nvim_lua'},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
+  },
+  mapping = cmp.mapping.preset.insert({
+    -- confirm completion item
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- toggle completion menu
+    ['<C-e>'] = cmp_action.toggle_completion(),
+
+    -- tab complete
+    ['<Tab>'] = cmp_action.tab_complete(),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+    -- navigate between snippet placeholder
+    ['<C-d>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+    -- scroll documentation window
+    ['<C-f>'] = cmp.mapping.scroll_docs(5),
+    ['<C-u>'] = cmp.mapping.scroll_docs(-5),
+  }),
+})
 
 -- for formatting files manually
 vim.api.nvim_create_user_command('Fmt',
@@ -77,6 +116,17 @@ vim.api.nvim_create_user_command('Format',
     {})
 
 -- for toggling the diagnostic virtual text to reduce clutter
+local function virtual_text_on()
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = false,
+        })
+    vim.g.diagnostic_virtual_txt = true
+    vim.diagnostic.show(nil, 0, nil, { virtual_text = true })
+end
 vim.g.diagnostic_virtual_txt = true
 virtual_text_on()
 function _G.toggle_virtual_txt()
@@ -96,7 +146,6 @@ function _G.toggle_virtual_txt()
         print('Diagnostics visible')
     end
 end
-
 vim.keymap.set('n', '<leader>tt', ':call v:lua.toggle_virtual_txt()<CR>', { silent = true, noremap = true })
 
 -- manual Zig ZLS setup
--- lua/jman4747/packer.lua
+++ lua/jman4747/packer.lua
@@ -1,12 +1,12 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
-		requires = { {'nvim-lua/plenary.nvim'} }
+		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
 	use({ 'rose-pine/neovim', as = 'rose-pine' })
 	use {
@@ -28,41 +28,35 @@ return require('packer').startup(function(use)
 			ts_update()
 		end,
 	}
-    -- For Zig lang
-    use('ziglang/zig.vim')
-    -- end for Zig lang
+	-- For Zig lang
+	use('ziglang/zig.vim')
+	-- end for Zig lang
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
 	use {
 		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
+		branch = 'v3.x',
 		requires = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{                                      -- Optional
-			'williamboman/mason.nvim',
-			run = function()
-				pcall(vim.cmd, 'MasonUpdate')
-			end,
-		},
-		{'williamboman/mason-lspconfig.nvim'}, -- Optional
+			{ 'neovim/nvim-lspconfig' }, -- Required
+			{ 'williamboman/mason.nvim' }, -- Optional
+			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-		-- Autocompletion
-		{'hrsh7th/nvim-cmp'},         -- Required
-		{'hrsh7th/cmp-nvim-lsp'},     -- Required
-		{'hrsh7th/cmp-buffer'},       -- Optional
-		{'hrsh7th/cmp-path'},         -- Optional
-		{'saadparwaiz1/cmp_luasnip'}, -- Optional
-		{'hrsh7th/cmp-nvim-lua'},     -- Optional
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' }, -- Required
+			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
+			{ 'hrsh7th/cmp-buffer' }, -- Optional
+			{ 'hrsh7th/cmp-path' }, -- Optional
+			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
+			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
-		-- Snippets
-		{'L3MON4D3/LuaSnip'},             -- Required
-		{'rafamadriz/friendly-snippets'}, -- Optional
-	    }
-    }
+			-- Snippets
+			{ 'L3MON4D3/LuaSnip' }, -- Required
+			{ 'rafamadriz/friendly-snippets' }, -- Optional
+		}
+	}
 
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
